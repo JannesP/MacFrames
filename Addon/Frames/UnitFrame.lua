25b78f0bd54be7e42885aefcb990318a97cb6158
JannesP <jannespeters.privat@gmail.com>
2020-09-25 13:25:09 +0200

    Change license to the GNU GPL and add file headers

--- Addon/Frames/UnitFrame.lua
+++ Addon/Frames/UnitFrame.lua
@@ -1,3 +1,21 @@
+--[[
+-- MacFrames - WoW Raid and Party Frames <https://github.com/JannesP/MacFrames>
+--Copyright (C) 2020  Jannes Peters
+--
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
 local ADDON_NAME, _p = ...;
 local LSM = LibStub("LibSharedMedia-3.0");
 local L = _p.L;
@@ -952,7 +970,7 @@ do
     local function ProjectNumberRange(value, oldMin, oldMax, newMin, newMax)
         return (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin; 
     end
-    
+
     function UnitFrame.SetHealth(self, health)
         local hb = self.healthBar;
         hb:SetValue(health);
